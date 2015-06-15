1d2926734663ad6b28e966f03fcf5b94a4d50911
Jessy Riordan <jessy.riordan@gmail.com>
2015-06-15 10:50:04 -0400

    playing around with $.when

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,5 @@
 ;(function(window) {
-  var $beer1 = $('.beer1');
+  var $beer1 = $('.beer1'),
       $beer2 = $('.beer2'),
       $beer3 = $('.beer3'),
       $beer4 = $('.beer4'),
@@ -9,9 +9,38 @@
       $beer3info = $('.beer3info'),
       $beer4info = $('.beer4info'),
       $beer5info = $('.beer5info');
+      // $percentage = $('.percentage');
 
-  $.getJSON('../api/styles.json')
-    .then (function(styles) {
+// $.when(
+//   $.getJSON('../api/styles.json')
+//   // $.getJSON('../api/calc.json')
+// ).then(function(styles){
+//   console.log(styles);
+//   $beer1.text(styles.data[0].name);
+//
+//   $beer2.text(styles.data[1].name);
+//
+//   $beer3.text(styles.data[2].name);
+//
+//   $beer4.text(styles.data[3].name);
+//
+//   $beer5.text(styles.data[4].name);
+//
+//   $beer1info.text(styles.data[0].description);
+//
+//   $beer2info.text(styles.data[1].description);
+//
+//   $beer3info.text(styles.data[2].description);
+//
+//   $beer4info.text(styles.data[3].description);
+//
+//   $beer5info.text(styles.data[4].description);
+//
+//   // $percentage.text(calc.percentage);
+// });
+
+$.getJSON('../api/styles.json')
+  .then (function(styles) {
       console.log(styles);
 
       $beer1.text(styles.data[0].name);
@@ -34,6 +63,11 @@
 
       $beer5info.text(styles.data[4].description);
 
-  })
+  });
+
+  // $.getJSON('../api/calc.json')
+  //   .then (function(calc) {
+  //     $percentage.text(calc.percentage);
+  //   });
 
 })(window);
